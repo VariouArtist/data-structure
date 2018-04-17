b2649a1ebc5585a8bceea8c2290fdb231b4b05ed
VariouArtist <34834710+VariouArtist@users.noreply.github.com>
2018-04-17 21:09:57 +0800

    Vector.h

--- Vector/bracket.h
+++ Vector/bracket.h
@@ -5,7 +5,7 @@ T& Vector<T>::operator[]( Rank r ) //重载下标操作符
 }
 
 const template <typename T> 
-T& Vector<T>::operator[] ( Rank r ) const //仅限于做右值
+T& Vector<T>::operator[]( Rank r ) const //仅限于做右值
 {
 	return _elem[r];
 }
